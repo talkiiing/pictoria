4d1c2aea8f7e6b11575ba8a5afbe561c7630db29
Виталий Лёвкин <unf.hwm@ya.ru>
2021-11-05 19:59:47 +0700

    fix: cors

--- src/api/index.js
+++ src/api/index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const multer = require('multer')
+const cors = require('cors')
 
 const uploadController = require('./upload')
 const getController = require('./get')
@@ -7,6 +8,8 @@ const getController = require('./get')
 module.exports = () => {
   const app = express()
   const upload = multer()
+
+  app.use(cors())
 
   app.post('/upload', upload.single('image'), uploadController(app))
   app.get('/get/:id', getController(app))
